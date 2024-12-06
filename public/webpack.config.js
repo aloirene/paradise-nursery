4180387db61c0bc64e137a4d3580e96cc83ce1c0
aloirene <alomandell96@gmail.com>
2024-12-06 16:50:04 +0000

    Initial commit for Paradise Nursery app

---
+++ public/webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    open: true
+  }
+};
